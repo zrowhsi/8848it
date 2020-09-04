3c47758cfecafa80f01ffe34de60764da13e6ed5
ishwor budhathoki <zrowhsi@gmail.com>
2020-09-03 20:18:42 -0600

    initialize git

---
+++ src/main/java/com/infotech/blog/Application.java
@@ -0,0 +1,15 @@
+package com.infotech.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("start running...");
+		System.out.println("hello world");
+	}
+
+}
